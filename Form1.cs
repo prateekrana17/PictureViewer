7e35ae78664a5c63c46a5a7c20faf36185a9fff2
prateekrana17 <66944558+prateekrana17@users.noreply.github.com>
2021-01-25 14:39:37 +0000

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PictureVIewer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            }
+            else
+            {
+                pictureBox1.SizeMode = PictureBoxSizeMode.Normal;
+            }
+        }
+
+        private void showButton_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() ==DialogResult.OK)
+            {
+                pictureBox1.Load(openFileDialog1.FileName);
+            }
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            //clear the picture
+            pictureBox1.Image = null;
+        }
+
+        private void backgroundButton_Click(object sender, EventArgs e)
+        {
+            //show the color dialog box. If the user clicks OK, change the background color
+            if (colorDialog1.ShowDialog()==DialogResult.OK)
+            {
+                pictureBox1.BackColor = colorDialog1.Color;
+            }
+        }
+
+        private void closeButton_Click(object sender, EventArgs e)
+        {
+            //close the form
+            this.Close();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
